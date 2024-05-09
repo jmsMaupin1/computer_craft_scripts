1074094d56d89d5c515655ba1e6512095b0bda34
JT <jmsMaupinRC@gmail.com>
2024-05-09 02:06:12 -0400

    fix textutils method

--- package_manager.lua
+++ package_manager.lua
@@ -26,7 +26,7 @@ local function get_available_scripts()
 	end
 
 	local sRes = res.readAll()
-	local files_table = textutils.serialize(sRes)
+	local files_table = textutils.unserializeJSON(sRes)
 	for i = 1,#files_table["tree"] do
 		print(files_table["tree"][i]["path"])
 	end
