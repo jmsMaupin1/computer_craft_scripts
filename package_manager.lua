1f47a8bec1031456e49749b39c149726f223c1ab
JT <jmsMaupinRC@gmail.com>
2024-05-09 04:00:58 -0400

    get ability to get all branches for a given repo

--- package_manager.lua
+++ package_manager.lua
@@ -3,6 +3,7 @@ local function printUsage()
 	print("Usage:")
 	print("<package_manager scripts name> i <script_name>")
 	print("<package_manager scripts name> l")
+	print("<package_manager scripts name> b")
 	print("<package_manager scripts name> --config.user <user_name>")
 	print("<package_manager scripts name> --config.repo <repo_name>")
 	print("<package_manager scripts name> --config.branch <branch_name>")
@@ -10,6 +11,7 @@ local function printUsage()
 end
 
 local CONFIG_NAME = "jpm.json"
+local GITHUB_API_BASE = "https://api.github.com"
 
 local jpm_config = {
 	user="jmsMaupin1",
@@ -64,9 +66,23 @@ local function strip_file_ext(file_name)
 	return file_name:match("(.+)%..+$") or file_name
 end
 
+local function get_all_branches()
+	local url = GITHUB_API_BASE .. "/repos" .. "/" .. jpm_config.user .. "/" .. jpm_config.repo .. "/branches"
+
+	local res = http.get(url, nil, true)
+	if not res then
+		print("failed to get branches")
+	end
+
+	local sRes = res.readAll()
+	local branch_table = textutils.unserializeJSON(sRes)
+	for i = 1,#branch_table do
+		print(branch_table[i])
+	end
+end
+
 local function get_available_scripts()
-	print("connecting to github to grab scripts")
-	local url = "https://api.github.com/repos/" .. jpm_config.user .. "/" .. jpm_config.repo .. "/git/trees/" .. jpm_config.branch .. "?recursive=1"
+	local url = GITHUB_API_BASE .. "/" .. jpm_config.user .. "/" .. jpm_config.repo .. "/git/trees/" .. jpm_config.branch .. "?recursive=1"
 
 	local res = http.get(url, nil, true)
 	if not res then
@@ -132,6 +148,10 @@ end
 if tArgs[1] == "l" then
 	get_available_scripts()
 	return nil
+end
+
+if tArgs[1] == "b" then
+	get_all_branches()
 end
 
 if tArgs[1] == "config.user" then
