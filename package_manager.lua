ce539a5249a3837ba416206fb85e6ddae3b67a1e
James Maupin <jmsMaupinRC@gmail.com>
2024-05-10 02:38:10 -0400

    Package manager (#2)
    * Begin working on package manager
    
    * update url for listing to githubs tree api and grabbed file names
    
    * fix spelling mistake
    
    * fix textutils method
    
    * remove extensions and filter out readme
    
    * Add function to strip file extensions
    
    * make package_manager configurable
    
    * Save config to a file and load the config from file
    
    * fix read_file function to properly check if file exists
    
    * fix a few errors in code
    
    * get ability to get all branches for a given repo
    
    * make branch lister print out names of branches
    
    * fix file reading to use sPath

--- package_manager.lua
+++ package_manager.lua
@@ -3,7 +3,21 @@ local function printUsage()
 	print("Usage:")
 	print("<package_manager scripts name> i <script_name>")
 	print("<package_manager scripts name> l")
+	print("<package_manager scripts name> b")
+	print("<package_manager scripts name> --config.user <user_name>")
+	print("<package_manager scripts name> --config.repo <repo_name>")
+	print("<package_manager scripts name> --config.branch <branch_name>")
+	print("<package_manager scripts name> --config")
 end
+
+local CONFIG_NAME = "jpm.json"
+local GITHUB_API_BASE = "https://api.github.com"
+
+local jpm_config = {
+	user="jmsMaupin1",
+	repo="computer_craft_scripts",
+	branch="main"
+}
 
 local tArgs = { ... }
 if #tArgs < 1 then
@@ -16,23 +30,69 @@ if not http then
 	print("set http_enable to true in ComputerCraft.cfg")
 end
 
+local function read_file(file_name)
+	local sPath = shell.resolve(file_name)
+	local file = fs.open(sPath, "r")
+
+	if not file then
+		return nil, "file not found"
+	end
+
+	return file.readAll(), nil
+end
+
+local function write_file(file_name, contents)
+	local sPath = shell.resolve(file_name)
+	local file = fs.open(sPath, "wb")
+	file.write(contents)
+	file.close()
+end
+
+local function load_config()
+	local config, err = read_file(CONFIG_NAME)
+
+	if err then
+		return jpm_config
+	end
+
+	return textutils.unserializeJSON(config)
+end
+
+local function save_config()
+	write_file(CONFIG_NAME, textutils.serializeJSON(jpm_config))
+end
+
 local function strip_file_ext(file_name)
 	return file_name:match("(.+)%..+$") or file_name
 end
 
-local function get_available_scripts()
-	print("connecting to github to grab scripts")
-	url = "https://api.github.com/repos/jmsMaupin1/computer_craft_scripts/git/trees/main?recursive=1"
+local function get_all_branches()
+	local url = GITHUB_API_BASE .. "/repos" .. "/" .. jpm_config.user .. "/" .. jpm_config.repo .. "/branches"
 
 	local res = http.get(url, nil, true)
 	if not res then
-		print("get fucked")
+		print("failed to get branches")
+	end
+
+	local sRes = res.readAll()
+	local branch_table = textutils.unserializeJSON(sRes)
+	for i = 1,#branch_table do
+		print(branch_table[i].name)
+	end
+end
+
+local function get_available_scripts()
+	local url = GITHUB_API_BASE .. "/" .. jpm_config.user .. "/" .. jpm_config.repo .. "/git/trees/" .. jpm_config.branch .. "?recursive=1"
+
+	local res = http.get(url, nil, true)
+	if not res then
+		printError("Failed getting scripts")
 	end
 
 	local sRes = res.readAll()
 	local files_table = textutils.unserializeJSON(sRes)
 	for i = 1,#files_table["tree"] do
-		file_name = files_table["tree"][i]["path"]
+		local file_name = files_table["tree"][i]["path"]
 		if not string.find(file_name, "README") then
 			print(strip_file_ext(file_name))
 		end
@@ -41,7 +101,7 @@ end
 
 local function get_script(file_name)
 	print("connecting to github to grab " .. file_name .. "... ")
-	url = "https://raw.githubusercontent.com/jmsMaupin1/computer_craft_scripts/main/"  .. file_name .. ".lua"
+	local url = "https://raw.githubusercontent.com/" .. jpm_config.user .. "/" .. jpm_config.repo .. "/" .. jpm_config.branch .. "/"  .. file_name .. ".lua"
 
 	local ok, err = http.checkURL(url)
 	if not ok then
@@ -65,16 +125,15 @@ local function get_script(file_name)
 end
 
 local function install(script_name, file_name)
-	local sPath = shell.resolve(file_name)
 	local res = get_script(script_name)
-
-	local file = fs.open(sPath, "wb")
-	file.write(res)
-	file.close()
-
+	write_file(file_name, res)
 	print("Downloaded script as " .. file_name)
 end
 
+jpm_config = load_config()
+
+-- Theres definitely a better way to deal with arguments
+-- this is very quick and dirty for now
 if tArgs[1] == "i" then
 	local file_name = tArgs[2] .. ".lua"
 	if tArgs[3] then
@@ -89,3 +148,35 @@ if tArgs[1] == "l" then
 	get_available_scripts()
 	return nil
 end
+
+if tArgs[1] == "l" then
+	get_available_scripts()
+	return nil
+end
+
+if tArgs[1] == "b" then
+	get_all_branches()
+end
+
+if tArgs[1] == "config.user" then
+	jpm_config.user = tArgs[2]
+	save_config()
+	return nil
+end
+
+if tArgs[1] == "config.repo" then
+	jpm_config.repo = tArgs[2]
+	save_config()
+	return nil
+end
+
+if tArgs[1] == "config.branch" then
+	jpm_config.branch = tArgs[2]
+	save_config()
+	return nil
+end
+
+if tArgs[1] == "config" then
+	print(textutils.serialize(jpm_config))
+end
+
